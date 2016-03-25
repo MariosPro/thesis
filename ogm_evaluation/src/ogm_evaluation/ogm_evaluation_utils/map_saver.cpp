68df3d070f1f104e2d9a8869e4e2d5ddf35b3b49
Marios Protopapas <protopapasmarios@gmail.com>
2016-03-25 22:09:26 +0200

    Add ros MapGenerator class to save ogm to image formats

---
+++ ogm_evaluation/src/ogm_evaluation/ogm_evaluation_utils/map_saver.cpp
@@ -0,0 +1,142 @@
+/******************************************************************************
+   OGM Validator - Occupancy Grid Map Validator
+   Copyright (C) 2015 OGM Validator
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+   Authors :
+   * Marios Protopapas, protopapasmarios@gmail.com
+******************************************************************************/
+
+#include <cstdio>
+#include <opencv2/opencv.hpp>
+#include "ros/ros.h"
+#include "ros/console.h"
+#include <ros/package.h>
+#include "nav_msgs/GetMap.h"
+#include "tf/LinearMath/Matrix3x3.h"
+#include "geometry_msgs/Quaternion.h"
+
+/**
+ * @brief Map generation node.
+ */
+class MapGenerator 
+{
+
+  public:
+    MapGenerator(const std::string& mapname) : mapname_(mapname), saved_map_(false)
+    {
+      ros::NodeHandle n;
+      ROS_INFO("Waiting for the map");
+      map_sub_ = n.subscribe("slam/occupancyGridMap", 1, &MapGenerator::mapCallback, this);
+    }
+
+    void mapCallback(const nav_msgs::OccupancyGridConstPtr& map)
+    {
+      ROS_INFO("Received a %d X %d map @ %.3f m/pix",
+               map->info.width,
+               map->info.height,
+               map->info.resolution);
+
+      std::string packagePath = ros::package::getPath("ogm_resources");
+
+      std::string mapdatafile = packagePath + "/maps/" + mapname_ + ".png";
+   
+      cv::Mat im(map->info.height, map->info.width, CV_8UC1);
+    
+    if (map->info.height*map->info.width != map->data.size())
+    {
+        ROS_ERROR("Data size doesn't match width*height: width = %d, height = %d, data size = %zu", map->info.width, map->info.height, map->data.size());
+    }
+    
+ 
+    for (size_t i = 0; i < map->info.height * map->info.width; i++)
+    {
+        if (map->data.at(i) == -1)
+        {
+            im.data[i] = 127;
+        }
+        else
+        {
+          im.data[i] = (100.0 - map->data.at(i)) / 100.0 * 255.0;
+        }
+    }
+    cv::Mat flipped;
+    cv::flip(im, flipped, 0);
+    cv::imwrite(mapdatafile, flipped);
+
+
+      std::string mapmetadatafile = packagePath + "/maps/" + mapname_ + ".yaml";
+      ROS_INFO("Writing map occupancy data to %s", mapmetadatafile.c_str());
+      FILE* yaml = fopen(mapmetadatafile.c_str(), "w");
+
+      geometry_msgs::Quaternion orientation = map->info.origin.orientation;
+      tf::Matrix3x3 mat(tf::Quaternion(orientation.x, orientation.y, orientation.z, orientation.w));
+      double yaw, pitch, roll;
+      mat.getEulerYPR(yaw, pitch, roll);
+
+      fprintf(yaml, "image: %s\nresolution: %f\norigin: [%f, %f, %f]\nnegate: 0\noccupied_thresh: 0.6\nfree_thresh: 0.3\n\n",
+              mapdatafile.c_str(), map->info.resolution, map->info.origin.position.x, map->info.origin.position.y, yaw);
+
+      fclose(yaml);
+
+      ROS_INFO("Done\n");
+      saved_map_ = true;
+    }
+
+    std::string mapname_;
+    ros::Subscriber map_sub_;
+    bool saved_map_;
+
+};
+
+#define USAGE "Usage: \n" \
+              "  map_saver -h\n"\
+              "  map_saver [-f <mapname>] [ROS remapping args]"
+
+int main(int argc, char** argv) 
+{
+  ros::init(argc, argv, "map_saver");
+  std::string mapname = "map";
+
+  for(int i=1; i<argc; i++)
+  {
+    if(!strcmp(argv[i], "-h"))
+    {
+      puts(USAGE);
+      return 0;
+    }
+    else if(!strcmp(argv[i], "-f"))
+    {
+      if(++i < argc)
+        mapname = argv[i];
+      else
+      {
+        puts(USAGE);
+        return 1;
+      }
+    }
+    else
+    {
+      puts(USAGE);
+      return 1;
+    }
+  }
+  
+  MapGenerator mg(mapname);
+
+  while(!mg.saved_map_ && ros::ok())
+    ros::spinOnce();
+
+  return 0;
+}
+
